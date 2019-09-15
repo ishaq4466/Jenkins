f59aa05fd67f5db403fbbc085e5f11d1b6cdc189
ishaq4466 <smartbuddy1995@gmail.com>
2019-09-15 18:00:53 +0530

    cahnges .yml

--- AutomateJenkinsContainer/run.sh
+++ AutomateJenkinsContainer/run.sh
@@ -3,14 +3,15 @@
 if [ "$1" == "deploy" ]
 then
 
+	echo "Building the image"
+
 	docker build -t jenkins-master .
 
-	echo "Create secrets.."
+	echo "Create secrets....."
 	# docker swarm init
 	echo "admin" | docker secret create jenkins-user -
 	echo "admin" | docker secret create jenkins-pass -
 	echo "Running the stack...."
-
 	# docker run -itd -u 0 -p 8080:8080 -p 50000:50000 -v "$HOME/jenkins-data":/var/jenkins_home --name j1 jenkins-master
 	# docker run -itd -p 8080:8080 -p 50000:50000 --name j1 jenkins-master
 	docker stack deploy -c jenkins.yml jenkins
