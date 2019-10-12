91551a7b49f9bb5c5400b3058db8fd47487e9b56
smartbuddy <smartbuddy1995@gmail.com>
2019-10-12 12:49:46 +0000

    Added ec2 termination Job

--- ec2TerminationJob/mainJobScript.sh
+++ ec2TerminationJob/mainJobScript.sh
@@ -1,25 +1,25 @@
-
 #!/bin/bash
 
 echo "********** EC2 state change job triggered *************"
+region=ap-south-1
 echo "Instance Id: $1 "
 echo "Instance state: $2"
+echo "region: $region"
 
 case $2 in
 "start")
 echo "Starting the instance as per parameter"
-echo aws ec2 start-instances --instance-id $1 --region ap-south-1 --dry-run
+echo aws ec2 start-instances --instance-id $1 --region $region --dry-run
 
 ;;
 "stop" | "Stop")
 echo "stoping the instance as per parameter"
-echo aws ec2 stop-instances --instance-id $1 --region ap-south-1 --dry-run
-
+aws ec2 stop-instances --instance-id $1 --region $region
 ;;
 
 "terminate" | "Terminate")
 echo "Terminating the instance as per parameter"
-echo aws ec2 terminate-instances --instance-id $1 --region ap-south-1 --dry-run
+echo aws ec2 terminate-instances --instance-id $1 --region $region --dry-run
 
 ;;
 *)
